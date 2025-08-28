abbb9754c35b8c30139d1240d0aa03838f01eea4
Sergio <bytiku@gmail.com>
2025-08-28 12:28:06 -0500

    COMANDOS

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hola mundo");
-
-
+        System.out.println("Estamos aprendiendo");
+        System.out.println("COMANDOS");
+        System.out.println("GIT STATUS");
+        System.out.println("GIT ADD. ");
+        System.out.println("GIT COMMIT -M "" ");
+        System.out.println("GIT BRANCH");
+        System.out.println("");
 
 
 
