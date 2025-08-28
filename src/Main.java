79176aafbab2b9afe1d58a4615f57bed351da1c3
Sergio <bytiku@gmail.com>
2025-08-28 12:34:09 -0500

    Commads

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,14 @@ public class Main {
         System.out.println("Estamos aprendiendo");
         System.out.println("COMANDOS");
         System.out.println("GIT STATUS");
-        System.out.println("GIT ADD. ");
-        System.out.println("GIT COMMIT -M "" ");
-        System.out.println("GIT BRANCH");
-        System.out.println("");
+        System.out.println("GIT ADD. AGREGA ARCHIVOS ");
+        System.out.println("GIT COMMIT -M GUARDA LOCAL ");
+        System.out.println("GIT PUSH ORIGIN - SUBE ARCHIVOS");
+        System.out.println("GIT BRANCH VER LAS RAMAS");
+        System.out.println("GIT CLONE CLONA PROYECTO");
+        System.out.println("GIT FETCH - RAMAS REMOTAS");
+        System.out.println("GIT SWITCH -C -CREAR Y POSICIONARME SOBRE LA RAMA");
+
 
 
 
